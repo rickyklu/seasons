68927c1ec7a337fa800f17b80759fc72acb61a6f
RCLU30 <15187974+RCLU30@users.noreply.github.com>
2019-06-10 08:33:48 -0400

    change to use hooks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
+const App = () => {
+	const [lat, setLat] = useState(null);
+	const [errorMessage, setErrorMessage] = useState('');
+
+	useEffect(() => {
+		window.navigator.geolocation.getCurrentPosition(
+			position => setLat(position.coords.latitude),
+			err => setErrorMessage(err.message)
+		);
+	}, []);
+	// empty [] -> runs the component once in lifecycle of loading (prevents componentDidUpdate)
+
+	let content;
+	if (errorMessage) {
+		content = <div>{errorMessage}</div>;
+	} else if (lat) {
+		content = <SeasonDisplay lat={lat} />;
+	} else {
+		content = <Spinner message="Please allow location request." />;
+	}
+
+	return <div className="border red">{content}</div>;
+};
+
+/*
 class App extends React.Component {
 	constructor(props){
 		super(props);
@@ -43,6 +68,6 @@ class App extends React.Component {
 	}
 	
 };
-
+*/
 
 ReactDOM.render(<App />, document.getElementById('root'));
